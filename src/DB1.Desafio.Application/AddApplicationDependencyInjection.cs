c9fd094b4be513b739bfbb23cd40b92f23919410
Ageu Machado <ageu.analista@gmail.com>
2024-08-24 18:13:17 -0300

    Ajustes na camada da aplicação

---
+++ src/DB1.Desafio.Application/AddApplicationDependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using DB1.Core.Mediator;
+using DB1.Desafio.Application.Commands.Empresa.Criar;
+using DB1.Desafio.Application.Commands.Empresa.Editar;
+using DB1.Desafio.Application.Commands.Empresa.ObterPorId;
+using DB1.Desafio.Application.Commands.Empresa.ObterTodos;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace DB1.Desafio.Application
+{
+    public static class ApplicationDependencyInjection
+    {
+        public static IServiceCollection AddApplicationDependencyInjection(this IServiceCollection services)
+        {
+            #region Eventos
+            services
+                .TryAddScoped<IMediatorHandler, MediatorHandler>();
+            #endregion
+
+            services
+            #region Empresa
+                .AddScoped<ICriarEmpresaUseCase, CriarEmpresaUseCase>()
+                .AddScoped<IEditarEmpresaUseCase, EditarEmpresaUseCase>()
+                .AddScoped<IObterPorIdEmpresaUseCase, ObterPorIdEmpresaUseCase>()
+                .AddScoped<IObterTodosEmpresaUseCase, ObterTodosEmpresaUseCase>();
+            #endregion
+
+            return services;
+        }
+    }
+}
