e228a18c07682ecb7ce57856f74fc6fce0d27361
Ageu Machado <ageu.analista@gmail.com>
2024-08-24 18:12:57 -0300

    Ajustes na camada de IOC

---
+++ src/DB1.Desafio.IOC/NativeInjectorConfig.cs
@@ -0,0 +1,19 @@
+﻿using DB1.Desafio.Application;
+using DB1.Desafio.Infra;
+using DB1.Desafio.Infra.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DB1.Desafio.IOC
+{
+    public static class NativeInjectorConfig
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddDbContext(configuration)
+                .AddInfraDependencyInjection()
+                .AddApplicationDependencyInjection();
+        }
+    }
+}
